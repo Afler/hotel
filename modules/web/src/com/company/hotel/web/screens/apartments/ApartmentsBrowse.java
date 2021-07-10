9322ab968e3faac584931de9bd0af54b4681ea3d
Lev <afler777@gmail.com>
2021-07-10 18:22:10 +0400

    implemented the logic of booking apartments

--- modules/web/src/com/company/hotel/web/screens/apartments/ApartmentsBrowse.java
+++ modules/web/src/com/company/hotel/web/screens/apartments/ApartmentsBrowse.java
@@ -1,8 +1,7 @@
 package com.company.hotel.web.screens.apartments;
 
-import com.company.hotel.service.ApartmentsService;
-import com.company.hotel.web.screens.client.ClientBrowse;
-import com.company.hotel.web.screens.registrationcard.RegistrationCardBrowse;
+import com.company.hotel.entity.Apartments;
+import com.company.hotel.entity.RegistrationCard;
 import com.company.hotel.web.screens.registrationcard.RegistrationCardEdit;
 import com.haulmont.cuba.core.global.DataManager;
 import com.haulmont.cuba.gui.Screens;
@@ -10,10 +9,8 @@ import com.haulmont.cuba.gui.components.Button;
 import com.haulmont.cuba.gui.components.GroupTable;
 import com.haulmont.cuba.gui.components.Table;
 import com.haulmont.cuba.gui.screen.*;
-import com.company.hotel.entity.Apartments;
 
 import javax.inject.Inject;
-import java.util.List;
 
 @UiController("hotel_Apartments.browse")
 @UiDescriptor("apartments-browse.xml")
@@ -22,24 +19,28 @@ import java.util.List;
 public class ApartmentsBrowse extends StandardLookup<Apartments> {
 
     @Inject
-    private ApartmentsService apartmentsService;
-
-    @Inject
     private Screens screens;
 
     @Inject
     private GroupTable<Apartments> apartmentsTable;
     @Inject
     private Button bookApartmentsBtn;
+    @Inject
+    private DataManager dataManager;
 
     @Subscribe("apartmentsTable")
     public void onApartmentsTableSelection(Table.SelectionEvent<Apartments> event) {
-        bookApartmentsBtn.setEnabled(true);
-        Apartments apartments = apartmentsTable.getSingleSelected();
+        bookApartmentsBtn.setEnabled(apartmentsTable.getSingleSelected().getIsFree());
     }
 
     @Subscribe("bookApartmentsBtn")
     public void onBookApartmentsBtnClick1(Button.ClickEvent event) {
-        List<Apartments> freeApartments = apartmentsService.getFreeApartments();
+        RegistrationCard registrationCard = dataManager.create(RegistrationCard.class);
+        Apartments apartments = apartmentsTable.getSingleSelected();
+        registrationCard.setApartments(apartments);
+        RegistrationCardEdit registrationCardEditScreen = screens.create(RegistrationCardEdit.class);
+        registrationCardEditScreen.setEntityToEdit(registrationCard);
+        registrationCardEditScreen.lockApartmentsField();
+        registrationCardEditScreen.show();
     }
 }
--- modules/web/src/com/company/hotel/web/screens/registrationcard/RegistrationCardEdit.java
+++ modules/web/src/com/company/hotel/web/screens/registrationcard/RegistrationCardEdit.java
@@ -1,7 +1,9 @@
 package com.company.hotel.web.screens.registrationcard;
 
+import com.company.hotel.entity.Apartments;
 import com.haulmont.cuba.gui.components.DateField;
 import com.haulmont.cuba.gui.components.HasValue;
+import com.haulmont.cuba.gui.components.PickerField;
 import com.haulmont.cuba.gui.screen.*;
 import com.company.hotel.entity.RegistrationCard;
 
@@ -17,6 +19,8 @@ public class RegistrationCardEdit extends StandardEditor<RegistrationCard> {
     private DateField<LocalDate> paymentDateField;
     @Inject
     private DateField<LocalDate> prepaymentDateField;
+    @Inject
+    private PickerField<Apartments> apartmentsField;
 
     @Subscribe("isPaymentField")
     public void onIsPaymentFieldValueChange(HasValue.ValueChangeEvent<Boolean> event) {
@@ -26,5 +30,13 @@ public class RegistrationCardEdit extends StandardEditor<RegistrationCard> {
     @Subscribe("isPrepaymentField")
     public void onIsPrepaymentFieldValueChange(HasValue.ValueChangeEvent<Boolean> event) {
         prepaymentDateField.setVisible(Boolean.TRUE.equals(event.getComponent().getValue()));
+    }
+
+    public void setApartmentsField(Apartments apartmentsField) {
+        this.apartmentsField.setValue(apartmentsField);
+    }
+
+    public void lockApartmentsField() {
+        apartmentsField.setEnabled(false);
     }
 }
